fb51de3ef333250fc29623335963a7e22f8eed87
Mike Ulvila <mikeulvila@me.com>
2016-07-04 06:38:04 -0500

    posting to api successfully

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
         <div className="form-group">
           <label>Content</label>
-          <textarea type="text" className="form-control" />
+          <textarea type="text" className="form-control" {...content} />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
@@ -24,10 +27,13 @@ class PostsNew extends Component {
   }
 }
 
+
+// connect: 1st argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd argument is mapStateToProps, 3rd is mapDispatchToProps
 export default reduxForm({
   form: 'PostsNewForm',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
 
 
 
