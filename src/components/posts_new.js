c44caaf4a4a475d7142725c6e77ec262d0ea1df2
Mike Ulvila <mikeulvila@me.com>
2016-07-04 05:52:25 -0500

    create PostsNew component and add route

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>Create Form</div>
+    );
+  }
+}
+
+export default PostsNew;
+
+
+
