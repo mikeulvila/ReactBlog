79c9d56645c48b48169a6c365b20718f59614e55
Mike Ulvila <mikeulvila@me.com>
2016-07-04 07:23:14 -0500

    navigating back to index onSubmit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmitForm(props) {
+    this.props.createPost(props)
+      .then(() => {
+        // blog post has been created, navigate user to index
+        // we navigate by calling this.context.router.push with the
+        // new path to navigate to
+        this.context.router.push('/');
+      });
+  }
+
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.props.createPost)}>
+      <form onSubmit={handleSubmit(this.onSubmitForm.bind(this))}>
         <h3>Create A New Post</h3>
         <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>Title</label>
